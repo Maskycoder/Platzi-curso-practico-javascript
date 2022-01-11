f088e0733b2b24f97a6cb1d104d243d15177b5c7
Masckycoder <maskycoder@gmail.com>
2022-01-11 22:35:03 +0100

    Creados los ficheros. Creada la funcion en JS para calcular la moda

---
+++ moda.js
@@ -0,0 +1,36 @@
+/* const lista1 = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1,
+]; */
+
+// calcular la moda de un array
+function calcularModa (lista){
+    const listaCount = {};
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+    
+    const listaArray = Object.entries(listaCount).sort(
+        function (a,b) {
+            return a[1] - b[1];
+        }
+    );
+    
+    const moda = listaArray[listaArray.length - 1];
+    return moda;
+}
