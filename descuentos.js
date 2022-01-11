07c25d21fafa2eb51d915e5de97934b4390c564e
Masckycoder <maskycoder@gmail.com>
2022-01-11 17:42:00 +0100

    Añadida la opción de calcular cupones de descuento adicionales

--- descuentos.js
+++ descuentos.js
@@ -1,19 +1,63 @@
+const coupons = [
+    {
+        name: "enero",
+        discount: 15,
+    },
+    {
+        name: "verano",
+        discount: 30,
+    },
+    {
+        name: "blackfriday",
+        discount: 25,
+    },
+    {
+        name: "navidad",
+        discount: 20,
+    },
+];
+    
+
 // cálculo del porcentaje a pagar 
-function porcentajeApagar(descuento) {
+function calcularDescuento(descuento) {
     return 100-descuento;
 }
 
 // Cálculo del precio final
 function precioFinal(precioOriginal, descuento){
-    return (precioOriginal * porcentajeApagar(descuento)/100);
+    return (precioOriginal * calcularDescuento(descuento)/100);
 }
 
+// Validar si existe el cupón
+function validarCupon(cupon) {
+    return coupon.name === cupon;
+}
 
 // integración con HTML
 function calcularPrecioConDescuento() {
     const inicial = parseFloat(document.getElementById("precioOriginal").value);
     const descuento = parseFloat(document.getElementById("descuento").value);
     const final = precioFinal(inicial, descuento);
-
+    
     document.getElementById("precioFinal").textContent = "El resultado es: " + final;
+}
+
+
+function calcularPrecioConCupon() {
+    const inicial = parseFloat(document.getElementById("precioOriginal").value);
+    var descuento = parseFloat(document.getElementById("descuento").value);
+    const couponValue = document.getElementById("cupon").value;
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    if(!userCoupon) {
+        alert('El cupón '+couponValue +' no existe');
+    } else {
+        descuento = descuento + userCoupon.discount;
+    }
+    const descuentoCupon = precioFinal(inicial, descuento);
+
+    document.getElementById("precioFinal").textContent = "El resultado es: " + descuentoCupon; 
 }
