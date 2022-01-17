609f40bd1aa56d25fb7b6e59eb4beb53a4d341da
Masckycoder <maskycoder@gmail.com>
2022-01-17 22:45:29 +0100

    Añadida la lógica del analisis de salarios

---
+++ salarios.js
@@ -0,0 +1,101 @@
+const colombia = [];
+colombia.push({
+  name: "Camila",
+  salary: 500,
+  expenses:200 ,
+});
+colombia.push({
+  name: "Nath",
+  salary: 1500,
+  expenses:500 ,
+});
+colombia.push({
+  name: "Luisa",
+  salary: 1800,
+  expenses:1000 ,
+});
+colombia.push({
+  name: "Laura",
+  salary: 1000,
+  expenses: 200,
+});
+colombia.push({
+  name: "Daniela",
+  salary: 2200,
+  expenses:1100 ,
+});
+colombia.push({
+  name: "Esperancita",
+  salary: 200,
+  expenses:50 ,
+});
+colombia.push({
+  name: "Carla",
+  salary: 500,
+  expenses: 300,
+});
+colombia.push({
+  name: "Antonieta",
+  salary: 1500,
+  expenses: 100 ,
+});
+colombia.push({
+  name: "Alicia",
+  salary: 1300,
+  expenses:1200 ,
+});
+colombia.push({
+  name: "Ana",
+  salary: 2400,
+  expenses: 800 ,
+});
+colombia.push({
+  name: "Julia",
+  salary: 3400,
+  expenses: 2400,
+});
+colombia.push({
+  name: "Rosa",
+  salary: 400,
+  expenses: 100,
+});
+colombia.push({
+  name: "Angélica",
+  salary: 400,
+  expenses:400 ,
+});
+colombia.push({
+  name: "Tatiana",
+  salary: 400,
+  expenses:200 ,
+});
+colombia.push({
+  name: "Lorena",
+  salary: 600,
+  expenses: 300,
+});
+colombia.push({
+  name: "Carolina",
+  salary: 1600,
+  expenses: 1000,
+});
+colombia.push({
+  name: "Fernanda",
+  salary: 2600,
+  expenses: 900 ,
+});
+colombia.push({
+  name: "Nora",
+  salary: 1000,
+  expenses: 200,
+});
+colombia.push({
+  name: "Gisselle",
+  salary: 2000,
+  expenses: 1500,
+});
+colombia.push({
+  name: "Bill Gates",
+  salary: 100000000,
+  expenses: 300000,
+});
